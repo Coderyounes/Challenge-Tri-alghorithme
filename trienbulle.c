b891877eeb672e20aedd90c0a7b7714472e5230b
Coderyounes <younesanas2021@gmail.com>
2024-09-06 16:26:22 +0100

    challenge 2 tri en bulle

--- trienbulle.c
+++ trienbulle.c
@@ -3,8 +3,8 @@
 void bubbleSort(int arr[], int size) {
     int i, j, temp;
 
-    for(i = 0; i < size - 1; i++) {
-        for(j = 0; j < size - i - 1; j++) {
+    for(i = 0; i < size ; i++) {
+        for(j = 0; j < size - i - 1 ; j++) {
             if(arr[j] > arr[j + 1]) {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
@@ -19,8 +19,8 @@ int main() {
     int arr[] = {12, 96, 480, 14, 2, 4, 9, 60};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    printArray(arr, size);
-    bubbleSort(arr, size);
-    printArray(arr, size);
+    printArray(arr, 8);
+    bubbleSort(arr, 8);
+    printArray(arr, 8);
     return (0);
 }
