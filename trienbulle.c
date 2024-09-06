83884cc7e6c706eb534bbe933c701636c826b0ed
Coderyounes <younesanas2021@gmail.com>
2024-09-06 11:35:05 +0100

    CHallenge 2: selection tri

--- trienbulle.c
+++ trienbulle.c
@@ -1,18 +1,9 @@
-#include <stdio.h>
-
-void printArray(int arr[], int size) {
-    int i;
-
-    for (i = 0; i < size;i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
+#include "main.h"
 
 void bubbleSort(int arr[], int size) {
     int i, j, temp;
 
-    for(i = 0; i < size; i++) {
+    for(i = 0; i < size - 1; i++) {
         for(j = 0; j < size - i - 1; j++) {
             if(arr[j] > arr[j + 1]) {
                 temp = arr[j];
@@ -27,7 +18,7 @@ void bubbleSort(int arr[], int size) {
 int main() {
     int arr[] = {12, 96, 480, 14, 2, 4, 9, 60};
     int size = sizeof(arr) / sizeof(arr[0]);
-    
+
     printArray(arr, size);
     bubbleSort(arr, size);
     printArray(arr, size);
---
+++ triparSelection.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b; 
+    *b = temp;
+}
+
+void selectionSort(int arr[], int size) {
+    int i, j, temp, min_idx;
+
+    for (i = 0; i < size; i++) {
+        min_idx = i;
+        for (j = i + 1; j < size; j++) {
+            if(arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+
+            if (min_idx != i) {
+                swap(&arr[min_idx], &arr[i]);
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {12, 96, 480, 14, 2, 4, 9, 60};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printArray(arr, size);
+    selectionSort(arr, size);
+    printArray(arr, size);
+    return (0);
+}
