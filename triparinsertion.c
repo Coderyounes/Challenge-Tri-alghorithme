8c6004db1c849ed6f2414a6df2c887ea59793f92
Coderyounes <younesanas2021@gmail.com>
2024-09-09 12:25:26 +0100

    challenge 2 : insertion alghorithme

--- triparinsertion.c
+++ triparinsertion.c
@@ -1,12 +1,27 @@
 #include "main.h"
 
+void insertionSort(int arr[], int n) {
+    int i, j, key;
 
-void insertionSort(int arr[], int size) {
-    
+    for(i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
 }
 
 
 int main() {
+    int arr[] = {12, 96, 480, 14, 2, 4, 9, 60};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
+    printArray(arr, size);
+    insertionSort(arr, size);
+    printArray(arr, size);
     return (0);
 }
